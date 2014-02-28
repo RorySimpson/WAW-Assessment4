f20eac0790e89efc234006ae96889dfc28225047
adam <hvfcrazy@hotmail.com>
2014-02-28 01:14:56 +0000

    Added Connection class and moved all connection related code to it

---
+++ Game/src/logicClasses/Connection.java
@@ -0,0 +1,33 @@
+package logicClasses;
+
+import java.util.HashMap;
+import java.io.*;
+import java.net.*;
+
+public class Connection {
+    private HashMap<String, String> scoreMap = new HashMap<String, String>();
+    
+    public HashMap<String,String> getScores(){
+        try {
+    	URL address = new URL("http://teamwaw.co.uk/whenPlanesCollide/connection.php");
+    	HttpURLConnection httpcon = (HttpURLConnection) address.openConnection(); 
+    	httpcon.addRequestProperty("User-Agent", "WhenPlanesCollide"); 
+    	BufferedReader in = new BufferedReader(new InputStreamReader(
+    	httpcon.getInputStream()));
+            
+    	String inputLine;
+    	while ((inputLine = in.readLine()) != null){
+    	    String[] parts = inputLine.split(":");
+    	    scoreMap.put(parts[0], parts[1]);
+    	}
+    	in.close();
+        } 
+        catch (MalformedURLException e) {
+    	e.printStackTrace();
+        } 
+        catch (IOException e) {
+    	e.printStackTrace();
+        }
+	return scoreMap;
+    }
+}
