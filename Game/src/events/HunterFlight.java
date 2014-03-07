75130299fc0c63632f7a2051d3fd41ffb8bf4b56
Adam Wright <aw973@york.ac.uk>
2014-03-07 14:49:25 -0800

    Modified HunterFlight; thinking of going to the Co-op.

--- Game/src/events/HunterFlight.java
+++ Game/src/events/HunterFlight.java
@@ -15,12 +15,16 @@ import logicClasses.*;
 public class HunterFlight {
 	
 	private static Image hunterFlightImage;
+	private static double gameScale = 1/1000.0;
+	private double turnRate = 0.9;
 	
-	public double x, y;
+	private double x, y;
 	private double currentHeading, targetHeading;
 	private int velocity;
 	private EntryPoint entryPoint;
 	private Flight victim;
+	private boolean turningLeft, turningRight;
+	
 	
 	public HunterFlight(Airspace airspace){
 		x = 0;
@@ -30,6 +34,8 @@ public class HunterFlight {
 		velocity = 500;
 		victim = generateVictim(airspace);
 		entryPoint = generateEntryPoint(airspace);
+		turningLeft = false;
+		turningRight = false;
 	}
 	
 	public Flight generateVictim(Airspace airspace){
@@ -46,7 +52,7 @@ public class HunterFlight {
 		return airspace.getListOfEntryPoints().get(randomNumber);
 	}
 	
-	public double calculateHeadingToVictim(Flight victim) {
+	public void calculateHeadingToVictim() {
 		
 		double deltaX;
 		double deltaY;
@@ -57,7 +63,83 @@ public class HunterFlight {
 		if (angle < 0) {
 			angle += 360;
 		}
-		return angle;
+		targetHeading = angle;
+	}
+	
+	public void updateXYCoordinates() {
+		
+		double vs = velocity *gameScale;
+		x += vs * Math.sin(Math.toRadians(currentHeading));
+		y -= vs * Math.cos(Math.toRadians(currentHeading));
+	}
+	
+	public void updateCurrentHeading() {
+		
+		if ((Math.round(this.targetHeading) <= Math.round(this.currentHeading) - 3 
+				|| Math.round(this.targetHeading) >= Math.round(this.currentHeading) + 3)) {
+			
+
+			/*
+			 * If plane has been given a heading so no turning direction specified,
+			 * below works out whether it should turn left or right to that heading
+			 */
+			
+			if(this.turningRight == false && this.turningLeft == false){
+
+				if (Math.abs(this.targetHeading - this.currentHeading) == 180) {
+					this.turningRight = true;
+				} 
+				
+				else if (this.currentHeading + 180 <= 359){
+					
+					if (this.targetHeading < this.currentHeading + 180 && this.targetHeading > this.currentHeading){
+						this.turningRight = true;
+					}
+					else {
+						this.turningLeft = true;
+					}
+				}
+				
+				else {
+					
+					if (this.targetHeading > this.currentHeading - 180 && this.targetHeading < this.currentHeading){
+						this.turningLeft = true;
+					}
+					else {
+						this.turningRight = true;
+					}
+				}
+
+			}
+			
+			// If plane is already turning right or user has told it to turn right
+			
+			if (this.turningRight == true) {
+				this.currentHeading += turnRate;
+				if (Math.round(this.currentHeading) >= 360 && this.targetHeading != 360) {
+					this.currentHeading = 0;
+				}
+			}
+
+			// If plane is already turning left or user has told it to turn left
+			
+			if (this.turningLeft == true) {
+				this.currentHeading -= turnRate;
+				if (Math.round(this.currentHeading) <= 0 && this.targetHeading != 0) {
+					this.currentHeading = 360;
+				}
+			}
+		}
+	}
+	
+	public void init(GameContainer gc) throws SlickException {
+		hunterFlightImage = new Image("res/graphics/flight_fast.png");
+	}
+	
+	public void update() {
+		updateCurrentHeading();
+		calculateHeadingToVictim();
+		updateXYCoordinates();
 	}
 	
 	public double getX(){
