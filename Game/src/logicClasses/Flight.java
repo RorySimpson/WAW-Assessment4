d6c59c9fb9f6b75faeb1f165cb06e1792e158e73
Rory1994 <rs1019@york.ac.uk>
2014-03-09 19:13:09 +0000

    Made flights descend faster

--- Game/src/logicClasses/Flight.java
+++ Game/src/logicClasses/Flight.java
@@ -15,47 +15,47 @@ public class Flight {
 
 	// FIELDS
 	private static Image 
-		regularFlightImage, slowFlightImage, fastFlightImage, shadowImage;
+	regularFlightImage, slowFlightImage, fastFlightImage, shadowImage;
 	private static double gameScale = 1/1000.0;
-	
+
 	private int
-		minVelocity = 200, maxVelocity = 400,
-		minAltitude = 2000, maxAltitude = 5000;
+	minVelocity = 200, maxVelocity = 400,
+	minAltitude = 2000, maxAltitude = 5000;
 	private double
-		accel = 20/60.0,
-		climbRate = 60/60.0,
-		turnRate = 0.9;	//20 mph per second
-	
+	accel = 20/60.0,
+	climbRate = 5,
+	turnRate = 0.9;	//20 mph per second
+
 	private int flightNumber;
 	private String flightName;
 	private double
-		x, y,
-		velocity, targetVelocity;
+	x, y,
+	velocity, targetVelocity;
 	private double
-		currentHeading, targetHeading;
+	currentHeading, targetHeading;
 	private int 
-		currentAltitude, targetAltitude;
-	
+	currentAltitude, targetAltitude;
+
 	private boolean turningRight, turningLeft;
-	
+
 	private Airspace airspace;
 	private FlightPlan flightPlan;
 	private boolean selected;
 	private final static int RADIUS = 30;
 	private int closestDistance = Integer.MAX_VALUE; // this is the maximum distance a plane
-									  // can be away from the waypoint once it has 
-									  // been checked that the plane is inside the waypoint
+	// can be away from the waypoint once it has 
+	// been checked that the plane is inside the waypoint
 	private int distanceFromWaypoint;
 	private double landingDescentRate = 0;
 
 	private boolean 
-		takingOff = false,
-		landing = false,
-		circling = false,
-		partCircling = false,
-		finalApproach = false;
+	takingOff = false,
+	landing = false,
+	circling = false,
+	partCircling = false,
+	finalApproach = false;
 
-	
+
 	// CONSTRUCTOR
 	public Flight(Airspace airspace) {
 		this.x = 0;
@@ -72,8 +72,8 @@ public class Flight {
 	}
 
 	// METHODS
-	
-	
+
+
 	/**
 	 * generateAltitude: Randomly assigns one of three different altitudes to a flight
 	 * @return A random altitude (either 28000, 29000 or 30000)
@@ -83,23 +83,23 @@ public class Flight {
 		if(getFlightPlan().getEntryPoint().isRunway()){
 			return 0;
 		}else{
-		Random rand = new Random();
-		int check = rand.nextInt(((maxAltitude-minAltitude)/1000) - 1);
-		return minAltitude + (check + 1) * 1000;
+			Random rand = new Random();
+			int check = rand.nextInt(((maxAltitude-minAltitude)/1000) - 1);
+			return minAltitude + (check + 1) * 1000;
 		}
 	}
 
-/**
- * calculateHeadingToFirstWaypoint: calculates heading between flight's current position and the first waypoint
- * in the flight's plan. The flight's current position will always be it's entrypoint because this method
- * is only called within the newFlight() function in airspace.
- * @param desX - The X coordinate of the waypoint
- * @param dexY - The Y coordinate of the waypoint
- * @return The heading between the flight and first waypoint.
- */
-	
+	/**
+	 * calculateHeadingToFirstWaypoint: calculates heading between flight's current position and the first waypoint
+	 * in the flight's plan. The flight's current position will always be it's entrypoint because this method
+	 * is only called within the newFlight() function in airspace.
+	 * @param desX - The X coordinate of the waypoint
+	 * @param dexY - The Y coordinate of the waypoint
+	 * @return The heading between the flight and first waypoint.
+	 */
+
 	public double calculateHeadingToNextWaypoint(double desX, double desY) {
-		
+
 		this.turningLeft = false;
 		this.turningRight = false;
 		double deltaX;
@@ -113,7 +113,7 @@ public class Flight {
 		}
 		return angle;
 	}
-	
+
 	/**
 	 * turnFlightLeft: sets the target heading to certain amount to to the left of the flight's current heading. 
 	 * When an angle is entered in the textfields found in Controls, the value is passed to turnFlightLeft. The
@@ -121,7 +121,7 @@ public class Flight {
 	 * towards it's target heading.
 	 * @param degreeTurnedBy - The amount of degrees you want to turn left by.
 	 */
-	
+
 	public void turnFlightLeft(int degreeTurnedBy) {
 
 		this.turningRight = false;
@@ -132,7 +132,7 @@ public class Flight {
 			this.targetHeading = 360 +this.targetHeading;
 		}
 	}
-	
+
 	/**
 	 * turnFlightRight: sets the target heading to certain amount to to the right of the flight's current heading. 
 	 * When an angle is entered in the textfields found in Controls, the value is passed to turnFlightRight. The
@@ -140,12 +140,12 @@ public class Flight {
 	 * towards it's target heading.
 	 * @param degreeTurnedBy - The amount of degrees you want to turn right by.
 	 */
-	
+
 	public void turnFlightRight(int degreeTurnedBy) {
 
 		this.turningLeft = false;
 		this.turningRight = true;
-		
+
 		this.targetHeading = Math.round(this.currentHeading) + degreeTurnedBy;
 		if(this.targetHeading >= 360){
 			this.targetHeading = this.targetHeading - 360;
@@ -153,7 +153,7 @@ public class Flight {
 
 
 	}
-	
+
 	/**
 	 * giveHeading: Changes the target heading to newHeading. Whenever a command is issued by the user to change the heading,
 	 * the method is passed the value of that command. The heading is always adjusted to a value between 0 and 359. This is 
@@ -167,7 +167,7 @@ public class Flight {
 		newHeading = newHeading % 360;
 		this.targetHeading = newHeading;
 	}
-	
+
 	public void incrementHeading() {
 		this.targetHeading+=1;
 		this.currentHeading+=1;
@@ -178,7 +178,7 @@ public class Flight {
 			this.currentHeading=0;
 		}
 	}
-	
+
 	public void decrementHeading() {
 		this.targetHeading-=1;
 		this.currentHeading-=1;
@@ -189,7 +189,7 @@ public class Flight {
 			this.currentHeading=360;
 		}
 	}
-	
+
 	/**
 	 * checkIfFlightAtWaypoint: checks whether a flight is close enough to the next waypoint in it's plan
 	 * for it to be considered at that waypoint. Update the closestDistance so that it knows how close the plane
@@ -197,32 +197,32 @@ public class Flight {
 	 * @param Waypoint - The next waypoint in the flight's plan.
 	 * @return True if flight is at it's next waypoint and it is moving away from that waypoint.
 	 */
-	
+
 	public boolean checkIfFlightAtWaypoint(Point waypoint) {
-		
-		
+
+
 		if (waypoint == airspace.getAirportLeft().getBeginningOfRunway() && this.landing == false){
 			return false;
 		}
-		
+
 		if (waypoint == airspace.getAirportRight().getBeginningOfRunway() && this.landing == false){
 			return false;
 		}
-		
-		
+
+
 		int distanceX;
 		int distanceY;
-		
+
 		distanceX = (int)(Math.abs(Math.round(this.x) - Math.round(waypoint.getX())));
 		distanceY = (int)(Math.abs(Math.round(this.y) - Math.round(waypoint.getY())));
 
 		distanceFromWaypoint = (int)Math.sqrt((int)Math.pow(distanceX,2) + (int)Math.pow(distanceY,2));
-		
+
 		// The plane is coming towards the waypoint
 		if (closestDistance > distanceFromWaypoint){
-				closestDistance = distanceFromWaypoint;
+			closestDistance = distanceFromWaypoint;
 		}	
-		
+
 		if ((distanceX <= RADIUS) && (distanceY <= RADIUS)) {
 			// The plane is going away from the way point
 			if (closestDistance < distanceFromWaypoint){
@@ -236,7 +236,7 @@ public class Flight {
 					}
 					else return true;					
 				}
-				
+
 				//for any non-exit waypoint
 				return true;
 			}
@@ -251,28 +251,28 @@ public class Flight {
 	public void resetMinDistanceFromWaypoint(){
 		closestDistance = Integer.MAX_VALUE;
 	}
-	
+
 	public void takeOff(){
 		takingOff = true;
 		setVelocity(100);
 		setTargetVelocity((minVelocity +maxVelocity) /2);
 		setTargetAltitude(minAltitude);
 	}
-	
+
 	public boolean altToLand(){
 		if(currentAltitude <= 720){
 			return true;
 		}else{
-			
+
 			return false;
 		}
 	}
 	public void land(){	
 		// if next point is an exit point
-		
-		
-		
-		
+
+
+
+
 		if (!landing){
 			if (this.airspace.getAirportRight().getLandingApproachArea()
 					.contains((float)this.x, (float)this.y) 
@@ -282,7 +282,7 @@ public class Flight {
 
 
 				this.landing 		= true;
-				
+
 				this.targetVelocity = velocity;
 
 
@@ -291,8 +291,8 @@ public class Flight {
 				this.airspace.getControls().setSelectedFlight(null);	
 
 			}
-			
-		
+
+
 			if (this.airspace.getAirportLeft().getLandingApproachArea()
 					.contains((float)this.x, (float)this.y) 
 					&& this.currentHeading >= 225 && this.currentHeading <= 305 && this.currentAltitude <= 2000
@@ -319,46 +319,46 @@ public class Flight {
 		}
 
 	}
-	
+
 	/** Calculates the rate at which a plane has to descend, given its current altitude, such
-     * that by the time it reaches the runway, its altitude is 0
-     * @return Rate at which plane needs to descend
-     */
+	 * that by the time it reaches the runway, its altitude is 0
+	 * @return Rate at which plane needs to descend
+	 */
 	public double findLandingDescentRate()
 	{
 		double rate;
 		double distanceFromRunway;
-		
+
 		if(this.flightPlan.getCurrentRoute().get(0) == this.airspace.getAirportLeft().getBeginningOfRunway() ){
 			//Find distance to runway waypoint
-			 distanceFromRunway 	=  Math.sqrt(Math.pow(this.x-this.airspace.getAirportLeft().getBeginningOfRunway().getX(), 2)
+			distanceFromRunway 	=  Math.sqrt(Math.pow(this.x-this.airspace.getAirportLeft().getBeginningOfRunway().getX(), 2)
 					+ Math.pow(this.y-this.airspace.getAirportLeft().getBeginningOfRunway().getY(), 2));
 
 		}
-		
+
 		else{
-			 distanceFromRunway 	=  Math.sqrt(Math.pow(this.x-this.airspace.getAirportRight().getBeginningOfRunway().getX(), 2)
+			distanceFromRunway 	=  Math.sqrt(Math.pow(this.x-this.airspace.getAirportRight().getBeginningOfRunway().getX(), 2)
 					+ Math.pow(this.y-this.airspace.getAirportRight().getBeginningOfRunway().getY(), 2));
-			
+
 		}
-		
-		
+
+
 		double descentPerPixel 		= this.currentAltitude/distanceFromRunway;
 
 		rate = descentPerPixel* (this.velocity * gameScale);
 
 		return rate;
 	}
-	
+
 	// DRAWING METHODS
-	
+
 	/**
 	 * drawFlight: draws the flight at it's current x,y and draws its information around within a circle.
 	 * Different images for the flight are used depending on how fast the plane is.
 	 * @param g - Graphics libraries required by slick2d.
 	 * @param gc - GameContainer required by slick2d.
 	 */
-	
+
 	public void drawFlight(Graphics g, GameContainer gc ){
 
 		g.setColor(Color.white);
@@ -396,7 +396,7 @@ public class Flight {
 
 		// Drawing Separation Circle
 
-		
+
 
 
 		// Drawing information around flight
@@ -411,21 +411,21 @@ public class Flight {
 					if (this.flightPlan.getCurrentRoute().get(0) == this.airspace.getAirportLeft().getBeginningOfRunway() && this.currentAltitude > 2000){
 						g.drawString("Lower Me",(int) this.x -29, (int)this.y-28);
 					}
-					
+
 					else if (this.flightPlan.getCurrentRoute().get(0) == this.airspace.getAirportLeft().getBeginningOfRunway() && this.currentAltitude <= 2000){
 						g.drawString("Line Me Up",(int) this.x -33, (int)this.y-28);
 					}
-					
+
 					else if (this.flightPlan.getCurrentRoute().get(0) == this.airspace.getAirportLeft().getBeginningOfRunway() && this.currentAltitude <= 2000){
 						g.drawString("Landing",(int) this.x -33, (int)this.y-28);
 					}
-					
+
 					else{
 						g.drawString("Aim: "+this.flightPlan.getPointByIndex(0).getPointRef(),(int) this.x -22, (int)this.y-28);
 					}
 
 				}
-				
+
 				g.drawOval((int) this.x - 50, (int) this.y - 50, 100, 100);
 			}
 
@@ -441,18 +441,18 @@ public class Flight {
 					if (this.flightPlan.getCurrentRoute().get(0) == this.airspace.getAirportLeft().getBeginningOfRunway()){
 						g.drawString("Land Me",(int) this.x -29, (int)this.y-28);
 					}
-					
+
 					else{
 						g.drawString("Aim: "+this.flightPlan.getPointByIndex(0).getPointRef(),(int) this.x -22, (int)this.y-28);
 					}
 				}
 				g.drawOval((int) this.x - 50, (int) this.y - 50, 100, 100);
 			}
-			
+
 			else if (this.currentAltitude == 0 && takingOff != true && this.flightPlan.getEntryPoint() == airspace.getAirportRight().getEndOfRunway()){
 				g.drawString("Take me off!",(int) this.x-80 , (int)this.y+28);
 			}
-			
+
 			else if (this.currentAltitude == 0 && takingOff != true && this.flightPlan.getEntryPoint() == airspace.getAirportLeft().getEndOfRunway()){
 				g.drawString("Take me off!",(int) this.x+50 , (int)this.y+28);
 			}
@@ -461,11 +461,11 @@ public class Flight {
 		g.setWorldClip(0, 0, Game.MAXIMUMWIDTH, Game.MAXIMUMHEIGHT);
 
 	}
-	
 
-	
+
+
 	// UPDATE METHODS
-	
+
 	/**
 	 * updateXYCoordinates: updates the x and y values of the plane depending on it's velocity 
 	 * and it's current heading. The velocity of the plane is scaled so that it can be used for 
@@ -474,7 +474,7 @@ public class Flight {
 
 	public void updateXYCoordinates() {
 		double vs = velocity *gameScale;
-		
+
 		this.x += vs * Math.sin(Math.toRadians(currentHeading));
 
 		this.y -= vs * Math.cos(Math.toRadians(currentHeading));
@@ -486,9 +486,9 @@ public class Flight {
 	 * If target altitude is less than current altitude, decrease current altitude. If current altitude and
 	 * target altitude are the same, do nothing.
 	 */
-	
+
 	public void updateAltitude() {
-		
+
 		if(this.landingDescentRate != 0)
 		{
 			if(this.currentAltitude < 0)
@@ -514,7 +514,7 @@ public class Flight {
 			}
 		}
 	}
-	
+
 	/**
 	 * updateCurrentHeading(): Moves the current heading towards the target heading. If a user has issued
 	 * a heading but not specified what way to turn, this method will determine what way it would be quicker 
@@ -522,24 +522,24 @@ public class Flight {
 	 */
 
 	public void updateCurrentHeading() {
-	
+
 		if ((Math.round(this.targetHeading) <= Math.round(this.currentHeading) - 3 
 				|| Math.round(this.targetHeading) >= Math.round(this.currentHeading) + 3)) {
-			
+
 
 			/*
 			 * If plane has been given a heading so no turning direction specified,
 			 * below works out whether it should turn left or right to that heading
 			 */
-			
+
 			if(this.turningRight == false && this.turningLeft == false){
 
 				if (Math.abs(this.targetHeading - this.currentHeading) == 180) {
 					this.turningRight = true;
 				} 
-				
+
 				else if (this.currentHeading + 180 <= 359){
-					
+
 					if (this.targetHeading < this.currentHeading + 180 && this.targetHeading > this.currentHeading){
 						this.turningRight = true;
 					}
@@ -547,9 +547,9 @@ public class Flight {
 						this.turningLeft = true;
 					}
 				}
-				
+
 				else {
-					
+
 					if (this.targetHeading > this.currentHeading - 180 && this.targetHeading < this.currentHeading){
 						this.turningLeft = true;
 					}
@@ -559,9 +559,9 @@ public class Flight {
 				}
 
 			}
-			
+
 			// If plane is already turning right or user has told it to turn right
-			
+
 			if (this.turningRight == true) {
 				this.currentHeading += turnRate;
 				if (Math.round(this.currentHeading) >= 360 && this.targetHeading != 360) {
@@ -570,7 +570,7 @@ public class Flight {
 			}
 
 			// If plane is already turning left or user has told it to turn left
-			
+
 			if (this.turningLeft == true) {
 				this.currentHeading -= turnRate;
 				if (Math.round(this.currentHeading) <= 0 && this.targetHeading != 0) {
@@ -579,11 +579,11 @@ public class Flight {
 			}
 		}
 	}
-	
-	public void updateVelocity(){
-		
 
-		
+	public void updateVelocity(){
+
+
+
 		double dv = 0.01*(targetVelocity - velocity);
 		if (targetVelocity > velocity) {
 			dv = Math.min(dv , accel);
@@ -592,12 +592,12 @@ public class Flight {
 		}
 
 		velocity += dv;
-		
+
 		if (Math.abs(targetVelocity - velocity)< 0.5){
-			
+
 			velocity = targetVelocity;
 		}
-		
+
 		if (takingOff && (Math.abs(minVelocity - velocity)< 0.5)){
 			takingOff = false;
 		}
@@ -605,12 +605,12 @@ public class Flight {
 
 
 	// UPDATE, RENDER, INIT
-	
+
 	/**
 	 * init: initialises resources such as images.
 	 * @param gc - GameContainer required by slick2d.
 	 */
-	
+
 	public void init(GameContainer gc) throws SlickException {
 		if (regularFlightImage == null){
 			regularFlightImage = new Image("res/graphics/flight.png");
@@ -626,15 +626,15 @@ public class Flight {
 		}
 
 	}
-	
-	
-/**
- * Update: calls all the update functions.
- */
+
+
+	/**
+	 * Update: calls all the update functions.
+	 */
 
 	public void update(ScoreTracking score) {
-		
-		
+
+
 		this.updateVelocity();
 		this.updateCurrentHeading();
 		this.updateXYCoordinates();
@@ -644,35 +644,35 @@ public class Flight {
 		if(this.landing){
 			this.steerLandingFlight();
 		}
-						
-			
-		
-		
+
+
+
+
 	}
-	
-public boolean withinTolerance(double x1, double x2,double tolerance){
-	return Math.abs(x1 - x2) <= tolerance;
-}
-/**
- * render: draw's all elements of the flight and it's information.
- * @param g - Graphics libraries required by slick2d.
- * @param gc - GameContainer required by slick2d.
- */
+
+	public boolean withinTolerance(double x1, double x2,double tolerance){
+		return Math.abs(x1 - x2) <= tolerance;
+	}
+	/**
+	 * render: draw's all elements of the flight and it's information.
+	 * @param g - Graphics libraries required by slick2d.
+	 * @param gc - GameContainer required by slick2d.
+	 */
 
 
 	public void render(Graphics g, GameContainer gc) throws SlickException {
-		
+
 		this.drawFlight(g, gc);
 		this.flightPlan.render(g,gc);
-		
 
 
-		
+
+
 	}
-	
+
 
 	// MUTATORS AND ACCESSORS
-	
+
 
 	public double getX() {
 		return this.x;
@@ -721,14 +721,14 @@ public boolean withinTolerance(double x1, double x2,double tolerance){
 	public void setAltitude(int altitude) {
 		this.currentAltitude = altitude;
 	}
-	
+
 	public boolean isGrounded(){
 		return (getAltitude() ==0);
 	}
 	public Boolean isCommandable(){
 		return (!isGrounded() && !landing);
 	}
-	
+
 	public int getMinVelocity() {
 		return minVelocity;
 	}
@@ -798,45 +798,45 @@ public boolean withinTolerance(double x1, double x2,double tolerance){
 	public void setCurrentAltitude(int currentAltitude) {
 		this.currentAltitude = currentAltitude;
 	}
-	
+
 	public double getVelocity() {
 		return velocity;
 	}
-	
+
 	public void setVelocity(double velocity) {
 		this.velocity = velocity;
 	}
-	
+
 	public void setTargetVelocity(double velocity){
 		this.targetVelocity = velocity;
 	}
-	
+
 	public double getTargetVelocity(){
 		return targetVelocity;
 	}
 	public FlightPlan getFlightPlan() {
 		return flightPlan;
 	}
-	
+
 	public boolean isLanding(){
 		return landing;
 	}
-	
+
 	public boolean isTakingOff(){
 		return takingOff;
 	}
-	
+
 	public void setTakingOff(Boolean bool){
 		takingOff = bool;
-		
-	}
-	
 
-	
+	}
+
+
+
 	public Airspace getAirspace(){
 		return airspace;
 	}
-	
+
 	public boolean getSelected(){
 		return this.selected;
 	}
@@ -884,7 +884,7 @@ public boolean withinTolerance(double x1, double x2,double tolerance){
 	public void setLanding(boolean landing) {
 		this.landing = landing;
 	}
-	
+
 
 
 
