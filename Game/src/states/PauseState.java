a15919c9e793b453a80b289860741ed5e00e8c27
hk3380 <lp752@york.ac.uk>
2014-04-19 16:13:26 +0100

    few more comments

--- Game/src/states/PauseState.java
+++ Game/src/states/PauseState.java
@@ -13,24 +13,32 @@ import util.DeferredFile;
 
 public class PauseState extends BasicGameState {
 	
+	/* Images */
 	private static Image
 		nextPageButton, previousPageButton, menuButton, quitButton, backButton,
 		nextPageButtonHover, previousPageButtonHover, quitButtonHover, backButtonHover,
 		pauseBackgroundPage1, pauseBackgroundPage2;
-	//private static TrueTypeFont font;
 	
+	/* Page number as the pause menu has 2 pages of instructions */
 	private int pageNumber;
 
 	
+	/* Constructor so Eclipse doesn't complain */
 	public PauseState(int state) {
 		
 	}
 
 	
+	/**
+	 * Overriding the initialisation method
+	 */
 	@Override
 	public void init(GameContainer gc, StateBasedGame sbj) throws SlickException {
+
+		/* Starts with the first page */
 		pageNumber = 1;
 		
+		/* Deferred loading of all the needed images */
 		{
 			LoadingList loading = LoadingList.get();
 
@@ -108,69 +116,90 @@ public class PauseState extends BasicGameState {
 						
 	}
 	
+	/**
+	 * Override the rendering method
+	 */
 	@Override
-	public void render(GameContainer gc, StateBasedGame sbg, Graphics g) throws SlickException {
+	public void render(GameContainer gc, StateBasedGame sbg, Graphics g) 
+			throws SlickException {
 		
+        /* Get the mouse position */
 		int	posX = Mouse.getX(),
 			posY = stateContainer.Game.MAXIMUMHEIGHT -Mouse.getY();
 		
+		/* Draw page number 1 */
 		if (pageNumber == 1) {
 			
 			pauseBackgroundPage1.draw(0,0);
 			
+			/* Check for hovering buttons */
 			if (posX>1020 && posX<1150 && posY>270 && posY<330)
 				nextPageButtonHover.draw(1030,280);
 			else nextPageButton.draw(1030,280);
 			
 		}
+		/* Draw page number 2 */
 		else if (pageNumber == 2){
 						
 			pauseBackgroundPage2.draw(0,0);
 
+			/* Check for hovering buttons */
 			if (posX>50 && posX<240 && posY>280 && posY<320) 
 				previousPageButtonHover.draw(30,280);
 			else previousPageButton.draw(30,280);				
 		}
 		
+		/* Back button hovering */
 		if (posX>20 && posX<40 && posY>20 && posY<40) 
 			backButtonHover.draw(20,20);
 		else backButton.draw(20,20);
 		
+		/* Quit button hovering */
 		if (posX>1150 && posX<1170 && posY>550 && posY<580) 
 			quitButtonHover.draw(1150,550);
 		else quitButton.draw(1150,550);
 		
 	}
 	
+	/**
+	 * Override the update method
+	 */
 	@Override
 	public void update(GameContainer gc, StateBasedGame sbg, int delta) throws SlickException {
 		
+		/* Get mouse position */
 		int	posX = Mouse.getX(),
 			posY = stateContainer.Game.MAXIMUMHEIGHT -Mouse.getY();
 		
+		/* Get user input */
 		Input input = gc.getInput();
 		
+		/* If user presses P, show pause */
 		if(input.isKeyPressed(Input.KEY_P)) {
 			pageNumber = 1;
 			sbg.enterState(stateContainer.Game.PLAYSTATE);
 		}
 		
+        //check if buttons are pressed
 		if (input.isMousePressed(Input.MOUSE_LEFT_BUTTON)) {
-			//check if buttons are pressed
 			
+			/* If user pressed back, return to game */
 			if (posX>20 && posX<40 && posY>20 && posY<40) {					
 				pageNumber = 1;
 				sbg.enterState(stateContainer.Game.PLAYSTATE);
 			}		
 			
+			/* If user pressed quit, exit game */
 			if (posX>1150 && posX<1170 && posY>550 && posY<580) {
 				System.exit(0);
 			}
 
+			/* If user pressed next page, change page */
 			if (pageNumber == 1){
 				if((posX > 1030 && posX < 1193) && (posY > 280 && posY < 315)) 
 					pageNumber = 2;			
 			}
+			/* If user pressed previous page, change page */
 			else if (pageNumber == 2){
 				if (posX>30 && posX<241 && posY>280 && posY<315) 
 					pageNumber = 1;		
@@ -180,6 +209,9 @@ public class PauseState extends BasicGameState {
 		
 	}
 	
+	/**
+	 * Better state ID
+	 */
 	@Override
 	public int getID(){
 		return stateContainer.Game.PAUSESTATE;
